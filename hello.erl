05a0f6ec62e312a2f6c9b8c0f7b548f4fb6f0103
Sherief Farouk <sherief@stealth.local>
2019-06-27 14:57:21 -0700

    Added success() and failure().

--- hello.erl
+++ hello.erl
@@ -1,5 +1,8 @@
 -module(hello).
--export([abs_value/2]).
+-export([abs_value/2, success/0, failure/0]).
 
 abs_value(negative, X) -> -X;
 abs_value(positive, X) -> X.
+
+success() -> ok.
+failure() -> error.
